dc557b0457baadbc6e4e56d64398e43ed3561c2e
Vastavikrahul01 <165668531+Vastavikrahul01@users.noreply.github.com>
2024-07-02 21:25:59 +0530

    Create main.py

---
+++ main.py
@@ -0,0 +1,27 @@
+from pyrogram import Client
+from pyromod import listen
+from config import API_ID, API_HASH, BOT_TOKEN
+
+class Bot(Client):
+
+    def __init__(self):
+        super().__init__(
+            "techvj login",
+            api_id=API_ID,
+            api_hash=API_HASH,
+            bot_token=BOT_TOKEN,
+            plugins=dict(root="TechVJ"),
+            workers=50,
+            sleep_threshold=10
+        )
+
+      
+    async def start(self):
+            
+        await super().start()
+        print('Bot Started Powered By @Vastaviik')
+
+    async def stop(self, *args):
+
+        await super().stop()
+        print('Bot Stopped Bye')
