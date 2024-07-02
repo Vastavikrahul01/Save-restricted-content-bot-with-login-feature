cfc9818becb8a6d444e064dde14caec3e80e3a91
Vastavikrahul01 <165668531+Vastavikrahul01@users.noreply.github.com>
2024-07-02 21:21:45 +0530

    Create config.py

---
+++ config.py
@@ -0,0 +1,13 @@
+import os
+
+#Bot token @Botfather
+BOT_TOKEN = os.environ.get("BOT_TOKEN", "")
+
+#Your API ID from my.telegram.org
+API_ID = int(os.environ.get("API_ID", ""))
+
+#Your API Hash from my.telegram.org
+API_HASH = os.environ.get("API_HASH", "")
+
+#Database 
+DB_URI = os.environ.get("DB_URI", "")
